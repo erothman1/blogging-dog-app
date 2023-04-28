ccfd9886069faffb6f3239fbfb428c1bd8ed8531
Elva Rothman <erothman@tulane.edu>
2023-04-28 19:35:25 -0400

    wrote post request to log in a user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -23,5 +23,35 @@ router.post('/', async (req, res) => {
 })
 
 //Post request to login a user
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        })
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect username or password 🙈 Please try again!' })
+            return
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password)
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect username or password 🙈 Please try again!' })
+            return
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true
+
+            res.status(200).json({ user: userData, message: 'You are now logged in!' })
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
 
 //Post request to log out a user
