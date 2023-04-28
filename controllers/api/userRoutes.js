4da04735ec0a04f63e140215e295cb6ad17da8c7
Elva Rothman <erothman@tulane.edu>
2023-04-28 19:29:08 -0400

    wrote post request to create a new user

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,7 +3,25 @@ const { Comment, User, Post } = require('../../models')
 const withAuth = require('../../utils/auth')
 
 //Post request to create a new user
- 
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        })
+
+        req.session.save(() => {
+            req.session.loggedIn = true
+
+            res.status(200).json(userData)
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
 //Post request to login a user
 
 //Post request to log out a user
