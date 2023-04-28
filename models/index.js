1e084bfa774a069f5a14a9322a76b4474ae4028c
Elva Rothman <erothman@tulane.edu>
2023-04-27 22:11:45 -0400

    wrote model associations in index.js

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const User = require("./User")
+const Post = require("./Post")
+const Comment = require("./Comment")
+
+//user has many posts
+User.hasMany(Post, {
+    foreignKey: "user_id"
+})
+
+//post belongs to one user
+Post.belongsTo(User)
+
+//user has many comments
+User.hasMany(Comment, {
+    foreignKey: "user_id"
+})
+
+//comment belongs to one user
+Comment.belongsTo(User)
+
+//post has many comments
+Post.hasMany(Comment, {
+    foreignKey: "post_id"
+})
+
+//comment belongs to post
+Comment.belongsTo(Post)
