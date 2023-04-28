8a0ee17331b34b9d58c0e00cc064ec647c463d76
Elva Rothman <erothman@tulane.edu>
2023-04-27 21:37:35 -0400

    wrote comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,19 +1,15 @@
 const { Model, DataTypes } = require('sequelize')
 const sequelize = require('../config/connection')
 
-class Post extends Model {}
+class Comment extends Model {}
 
-Post.init(
+Comment.init(
     {
         id: {
             type: DataTypes.INTEGER,
             allowNull: false, 
             primaryKey: true,
             autoIncrement: true,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
         },
         content: {
             type: DataTypes.STRING,
@@ -26,13 +22,21 @@ Post.init(
                 model: "user",
                 key: "id"
             }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "post",
+                key: "id"
+            }
         }
     },
     {
         sequelize,
-        timestamps: true, //we want timestamps so that we can have date created next to each blog post 
+        timestamps: true, //we want timestamps so that we can have date created next to each comment
         freezeTableName: true,
         underscored: true,
-        modelName: 'post',
+        modelName: 'comment',
     }
 )
