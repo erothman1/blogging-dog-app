e231201e3efe8a6361980920321064daaf0fd1ab
Elva Rothman <erothman@tulane.edu>
2023-04-28 18:59:39 -0400

    pseudocoded get requests for homeroutes.js in controllers and created their respective handlebars files

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const { User, Post, Comment } = require('../models')
+//require middleware for if logged in
+
+//Get request to get all posts joined with user data, rendered on the homepage
+
+//Get request to get single post joined with user data and comment data, rendered on blogPost page
+//need middleware to require login before accessing this page
+
+// Get request to get single (logged in) user joined with their post data
+//need middleware to require login before accessing this page
+
+//Get request to render login page
+
