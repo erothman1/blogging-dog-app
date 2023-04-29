15bf6479d2c1b88c9d45917606e32743cec839ac
Elva Rothman <erothman@tulane.edu>
2023-04-29 19:49:24 -0400

    wrote and styled login page

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,17 @@
-//Function for fetch GET request for singular blog post
-// const singlePost = document.querySelector('.getBlogPost')
+//Show login form
+const loginButton = document.getElementById('login')
+const signupButton = document.getElementById('signup')
 
-// singlePost.addEventListener('click', async () => {
-//     const id = singlePost.getAttribute('data-id')
-//     const response = await fetch(`/blog/${id}`, {
-//         method: 'GET'
-//     })
-//     if (response.ok) {
-//         document.location.replace(`/blog/${id}`)
-//     } else {
-//         console.log(response.statusText)
-//     }
-// })
+loginButton.addEventListener('click', () => {
+    const loginForm = document.getElementById('loginForm')
+    loginForm.setAttribute("style", "display: block")
+    loginButton.setAttribute("style", "display:none")
+    signupButton.setAttribute("style", "display:none")
+})
+
+signupButton.addEventListener('click', () => {
+    const signupForm = document.getElementById('signupForm')
+    signupForm.setAttribute("style", "display: block")
+    loginButton.setAttribute("style", "display:none")
+    signupButton.setAttribute("style", "display:none")
+})
