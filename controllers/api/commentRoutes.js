ba4fd2f3be1db27c042203d5d3c8dda5018bac8a
Elva Rothman <erothman@tulane.edu>
2023-04-28 23:01:11 -0400

    wrote delete request to destroy a comment

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -52,5 +52,22 @@ router.put('/:id', withAuth, async (req, res) => {
 })
 
 //Delete request to delete existing comment 
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            }
+        })
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id!' })
+            return
+          }
+      
+          res.status(200).json(commentData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
