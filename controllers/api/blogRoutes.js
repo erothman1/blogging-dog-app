1f0772e68cc8cca8f666959099b224f9336a2c4f
Elva Rothman <erothman@tulane.edu>
2023-04-28 22:48:06 -0400

    wrote delete request to delete a blog post

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -5,10 +5,17 @@ const withAuth = require('../../utils/auth')
 //Post request to create new blog post (title and content)
 router.post('/', withAuth, async (req, res) => {
     try {
+        const user = await User.findOne({
+            where: {
+                username: req.session.username
+            }
+        })
+        const userId = user.id
+
         const newPost = await Post.create({
             title: req.body.title,
             content: req.body.content,
-            user_id: req.user_id
+            user_id: userId
         })
 
         res.status(200).json(newPost)
@@ -39,5 +46,22 @@ router.put('/:id', withAuth, async (req, res) => {
 })
 
 //Delete request to destroy existing blog post 
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+            }
+        })
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!' })
+            return
+          }
+      
+          res.status(200).json(postData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
