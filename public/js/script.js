406c73adb6efca73070206f729c5414e738be296
Elva Rothman <erothman@tulane.edu>
2023-04-29 20:37:11 -0400

    added functionality to login form with post request to login user

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
-//Show login form
 const loginButton = document.getElementById('login')
 const signupButton = document.getElementById('signup')
 
+//Show login form
 loginButton.addEventListener('click', () => {
     const loginForm = document.getElementById('loginForm')
     loginForm.setAttribute("style", "display: block")
@@ -9,9 +9,36 @@ loginButton.addEventListener('click', () => {
     signupButton.setAttribute("style", "display:none")
 })
 
+//Show sign up form
 signupButton.addEventListener('click', () => {
     const signupForm = document.getElementById('signupForm')
     signupForm.setAttribute("style", "display: block")
     loginButton.setAttribute("style", "display:none")
     signupButton.setAttribute("style", "display:none")
 })
+
+//Login request
+const loginFormHandler = async (event) => {
+    event.preventDefault()
+
+    const username = document.getElementById('username-login').value.trim()
+    const email = document.getElementById('email-login').value.trim()
+    const password = document.getElementById('password-login').value.trim()
+
+    if (username && email && password) {
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+        if (response.ok) {
+            document.location.replace('/')
+            console.log("LOGGED IN")
+        }else {
+            alert('Failed to login')
+        }
+    }
+}
+
+document.getElementById("loginSubmit").addEventListener("click", loginFormHandler)
