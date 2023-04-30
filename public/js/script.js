f75e8a0c581d72a58377eb06a030aec72f50377f
Elva Rothman <erothman@tulane.edu>
2023-04-29 20:44:20 -0400

    added functionality to signup form with post request to create a user

--- public/js/script.js
+++ public/js/script.js
@@ -42,3 +42,28 @@ const loginFormHandler = async (event) => {
 }
 
 document.getElementById("loginSubmit").addEventListener("click", loginFormHandler)
+
+//Signup request
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.getElementById('username-signup').value.trim();
+    const email = document.getElementById('email-signup').value.trim();
+    const password = document.getElementById('password-signup').value.trim();
+  
+    if (username && email && password) {
+      const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to sign up.');
+      }
+    }
+  };
+
+  document.getElementById("signup-submit").addEventListener("click", signupFormHandler)
