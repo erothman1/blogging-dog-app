8e3f68be54df8e607ed02abef811b93814cc6b08
Elva Rothman <erothman@tulane.edu>
2023-04-30 23:29:17 -0400

    wrote js for deleting a blog post

---
+++ public/js/deletePost.js
@@ -0,0 +1,16 @@
+const deleteButton = document.getElementById("delete-button")
+
+const deletePostHandler = async () => {
+    const postId = post.getAttribute("data-postid")
+
+    const response = await fetch(`/api/blog/${parseInt(postId)}`, {
+        method: "DELETE",
+    })
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to delete ppost');
+    }
+}
+
+deleteButton.addEventListener("click", deletePostHandler)
